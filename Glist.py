e0ad3088cc6339b377648139584ff49c8fb6027a
Glitch <78031997+B4H09@users.noreply.github.com>
2021-02-20 19:43:31 +0200

    Update Glist.py

--- Glist.py
+++ Glist.py
@@ -2,7 +2,7 @@ import os
 os.system('printf "\033[3;32m" ')
 
 os.system('printf "\033[3;36m"') 
-    os.system('figlet Glist')
+os.system('figlet Glist')
 
 list=input('\nEntre Name The List : \n')
 os.system('clear') 
